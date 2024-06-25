f71e264d9f625017bf12c7c07525d13084f5a10b
pojash <72611525+pojash@users.noreply.github.com>
2024-06-25 14:22:53 +0000

    Create an azure resource group using c# SDK

---
+++ package-dotnet/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
+using Azure.ResourceManager.Storage;
+using Azure.ResourceManager.Storage.Models;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Define the subscription ID and resource group details
+       // string subscriptionId = "b3371e36-ccf2-4e87-b2bd-f93452ebba34";
+        string resourceGroupName = "my-sdk-rg";
+      //  string storageAccountName = "himystorage7619";
+        string location = "westus";
+
+         // Authenticate the client
+        var credential = new DefaultAzureCredential();
+        //var armClient = new ArmClient(credential);
+        ArmClient client = new ArmClient(new DefaultAzureCredential());
+
+        
+        // Create or get access to resource geoup
+        SubscriptionResource subscription = await client.GetDefaultSubscriptionAsync();
+        ResourceGroupCollection resourceGroups = subscription.GetResourceGroups();
+        ResourceGroupData resourceGroupData = new ResourceGroupData(location);
+        ArmOperation<ResourceGroupResource> operation = await resourceGroups.CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, resourceGroupData);
+
+
+        Console.WriteLine($"Resource group '{resourceGroupName}' created.");
+
+        
+    }
+}
+
